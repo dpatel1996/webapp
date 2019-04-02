54fb7696c0133527515eed8b2ceba0382b33fde6
dpatel1996 <dpatel108@hawk.iit.edu>
2019-04-02 12:40:16 -0500

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php 
+
+$username = filter_input(INPUT_POST, 'username')';
+$password = filter_input(INPUT_POST, 'password');
+if (!empty($username)){
+if(!empty($password)){
+$host = "localhost";
+$dbusername = "root" 
+$dbpassword = "";
+$dbname = "account"}
+}
+?>
